134ecc5e914b1bc0f3d82f0ff7b714cdabeb7422
HG <HG@HG>
2022-09-21 13:45:37 +0100

    End debug

--- Catcher2022/go.php
+++ Catcher2022/go.php
@@ -404,9 +404,9 @@ class Epp
     public function getDropTime($domain)
     {
         // DEBUG
-        return (new DateTime("+2 minutes", new DateTimeZone('UTC')));
-
+        // return (new DateTime("+2 minutes", new DateTimeZone('UTC')));
         //
+        
         $checkDomainXML = '<?xml version="1.0" encoding="UTF-8" standalone="no"?>
 <epp xmlns="urn:ietf:params:xml:ns:epp-1.0">
   <command>
